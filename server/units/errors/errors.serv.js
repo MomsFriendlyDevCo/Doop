aaeb2256f103cbe1822605ff9a07da66653cfd9f
theServerGod <the.server.god@gmail.com>
2016-09-22 11:39:43 +1000

    REFACTOR: Updated back end error handler service

--- server/units/errors/errors.serv.js
+++ server/units/errors/errors.serv.js
@@ -1,7 +1,13 @@
 app.register('postServer', function(finish) {
+	// Handle 404
+	app.use(function(req, res) {
+		res.status(404).render('units/errors/404');
+	});
+
+	// Handle 500
 	app.use(function(err, req, res, next){
 		console.error(err.stack);
-		res.send(500, 'Something broke!').end();
+		res.status(500).render('units/errors/500');
 	});
 
 	finish();
