37d4daf8a169cb5c629b6f62f7ebf0b350c33245
Matt Carter <m@ttcarter.com>
2016-11-08 17:47:24 +1000

    REFACTOR: Login + Logout are now components

--- units/session/login.ctrl.js
+++ units/session/login.ctrl.js
@@ -1,31 +1,44 @@
 angular
 	.module('app')
-	.controller('LoginCtrl', function($rootScope, $auth, $session, $state) {
-		var $ctrl = this;
+	.config(function($stateProvider) {
+		$stateProvider
+			.state('session-login', {
+				url: '/login',
+				component: 'loginCtrl',
+				data: {
+					title: 'Login',
+				},
+			});
+	})
+	.component('loginCtrl', {
+		templateUrl: '/units/session/login.tmpl.html',
+		controller: function($rootScope, $auth, $session, $state) {
+			var $ctrl = this;
 
-		// Define visibility control for this controller
-		$auth.ensureUnauthenticated();
+			// Define visibility control for this controller
+			$auth.ensureUnauthenticated();
 
-		// Init scope variables
-		this.user = { username: '', password: '' };
+			// Init scope variables
+			this.user = { username: '', password: '' };
 
-		// Messages
-		$ctrl.error;
+			// Messages
+			$ctrl.error;
 
-		$ctrl.login = function(isValid) {
-			if (!isValid) return;
+			$ctrl.login = function(isValid) {
+				if (!isValid) return;
 
-			// Perform login
-			$session.login(this.user)
-				.then(_=> $rootScope.$broadcast('loginSuccess'))
-				.catch(err => {
-					$rootScope.$broadcast('loginFailure');
-					// Handle login-specifc errors the server may throw
-					if (err.error) {
-						$ctrl.error = res.error;
-					} else {
-						$ctrl.error = 'Could not login!';
-					}
-				});
-		};
+				// Perform login
+				$session.login(this.user)
+					.then(_=> $rootScope.$broadcast('loginSuccess'))
+					.catch(err => {
+						$rootScope.$broadcast('loginFailure');
+						// Handle login-specifc errors the server may throw
+						if (err.error) {
+							$ctrl.error = res.error;
+						} else {
+							$ctrl.error = 'Could not login!';
+						}
+					});
+			};
+		},
 	});
