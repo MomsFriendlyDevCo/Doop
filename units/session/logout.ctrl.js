49f7d6759ff3bf49cae2154a534bd35699568328
Matt Carter <m@ttcarter.com>
2016-11-08 17:47:24 +1000

    REFACTOR: Login + Logout are now components

--- units/session/logout.ctrl.js
+++ units/session/logout.ctrl.js
@@ -1,8 +1,21 @@
 angular
 	.module('app')
-	.controller('LogoutCtrl', function($session, $timeout, $window) {
-		$timeout(function() { // Wait a second for everything to settle then logout
-			$session.logout()
-				.then(_=> $window.location = '/');
-		}, 1000);
+	.config(function($stateProvider) {
+		$stateProvider
+			.state('session-logout', {
+				url: '/logout',
+				component: 'logoutCtrl',
+				data: {
+					title: 'Logout',
+				},
+			});
+	})
+	.component('logoutCtrl', {
+		templateUrl: '/units/session/logout.tmpl.html',
+		controller: function($session, $timeout, $window) {
+			$timeout(function() { // Wait a second for everything to settle then logout
+				$session.logout()
+					.then(_=> $window.location = '/');
+			}, 1000);
+		},
 	});
