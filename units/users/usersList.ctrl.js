2c7aaeb14adce4c88d0639a5db23029f2d240943
Matt Carter <m@ttcarter.com>
2016-11-08 14:59:12 +1000

    User list is now a component

--- units/users/usersList.ctrl.js
+++ units/users/usersList.ctrl.js
@@ -1,19 +1,32 @@
 angular
 	.module('app')
-	.controller('UsersListCtrl', function($scope, $location, $loader, $timeout, $toast, Users) {
-		var $ctrl = this;
+	.config(function($stateProvider) {
+		$stateProvider
+			.state('users-list', {
+				url: '/users',
+				component: 'usersListCtrl',
+				data: {
+					title: 'Manage Users',
+				},
+			})
+	})
+	.component('usersListCtrl', {
+		templateUrl: '/units/users/list.tmpl.html',
+		controller: function($scope, $location, $loader, $timeout, $toast, Users) {
+			var $ctrl = this;
 
-		// Data refresher {{{
-		$ctrl.users;
-		$ctrl.refresh = function() {
-			$loader.start($scope.$id, $ctrl.users === undefined);
+			// Data refresher {{{
+			$ctrl.users;
+			$ctrl.refresh = function() {
+				$loader.start($scope.$id, $ctrl.users === undefined);
 
-			Users.query().$promise
-				.then(data => $ctrl.users = data)
-				.catch($toast.catch)
-				.finally(() => $loader.stop($scope.$id));
-		};
-		// }}}
+				Users.query().$promise
+					.then(data => $ctrl.users = data)
+					.catch($toast.catch)
+					.finally(() => $loader.stop($scope.$id));
+			};
+			// }}}
 
-		$scope.$evalAsync($ctrl.refresh);
+			$scope.$evalAsync($ctrl.refresh);
+		},
 	});
