0f5d46784bf476950e2d50c28e33aeffa3a47291
Matt Carter <m@ttcarter.com>
2016-11-09 13:36:30 +1000

    Added user profile editing

---
+++ units/session/profile.ctrl.js
@@ -0,0 +1,46 @@
+angular
+	.module('app')
+	.config(function($stateProvider) {
+		$stateProvider
+			.state('users-profile', {
+				url: '/profile',
+				component: 'usersProfileCtrl',
+				data: {
+					title: 'Edit Your Profile',
+				},
+			})
+	})
+	.component('usersProfileCtrl', {
+		templateUrl: '/units/session/profile.tmpl.html',
+		controller: function($scope, $location, $loader, $session, $stateParams, $timeout, $toast, Users) {
+			var $ctrl = this;
+
+			// Data refresher {{{
+			$ctrl.meta;
+			$ctrl.user = $session.data;
+			$ctrl.refresh = function() {
+				if (!$ctrl.meta) { // Not asked the server for meta data yet
+					$loader.start($scope.$id, $ctrl.user === undefined);
+
+					Users.meta().$promise
+						.then(data => $ctrl.meta = data)
+						.finally(() => $loader.stop($scope.$id));
+				}
+			};
+			// }}}
+
+			// Save {{{
+			$ctrl.save = function() {
+				$loader.start($scope.$id);
+				Users.save({id: $session.data._id}, $ctrl.user).$promise
+					.then(_=> $toast.success('Profile saved'))
+					.then(_=> $location.path('/'))
+					.then(_=> $session.update())
+					.catch($toast.catch)
+					.finally(() => $loader.stop($scope.$id));
+			};
+			// }}}
+
+			$scope.$evalAsync($ctrl.refresh);
+		},
+	});
