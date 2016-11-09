0483eec884bd037c6e88c251c2f75770d57d1c16
Matt Carter <m@ttcarter.com>
2016-11-09 13:18:24 +1000

    Final tweaks to invite confirmation system

--- units/session/invite.ctrl.js
+++ units/session/invite.ctrl.js
@@ -18,17 +18,21 @@ angular
 			})
 	})
 	.component('usersInviteCtrl', {
-		templateUrl: '/units/users/invite.tmpl.html',
-		controller: function($scope, $location, $toast, Users) {
+		templateUrl: '/units/session/invite.tmpl.html',
+		controller: function($scope, $loader, $location, $toast, Users) {
 			var $ctrl = this;
 
 			$ctrl.user = {email: ''};
 			$ctrl.error;
 
 			$ctrl.invite = function() {
+				$loader.startBackground($scope.$id);
+
 				Users.invite({
 					emails: $ctrl.user.email.split(/[\s\n,]+/),
 				}).$promise
+					.then(_=> $loader.stop($scope.$id))
+					.then(_=> $toast.success('Invites sent'))
 					.then(data => $location.path('/users'))
 					.catch(res => $ctrl.error = res.data.error);
 			};
