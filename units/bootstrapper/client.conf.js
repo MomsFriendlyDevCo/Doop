cb54fc5743cea49251d18f2b2b84b11cab63ec76
Matt Carter <m@ttcarter.com>
2016-11-12 09:29:40 +1000

    Seperated router rules into their own .run() sequences - keeping track of exactly what sets needed what injections was getting annoying

--- units/bootstrapper/client.conf.js
+++ units/bootstrapper/client.conf.js
@@ -3,26 +3,46 @@
 */
 angular
 	.module('app')
+
+	// Anglar core config {{{
 	.config(function($httpProvider) {
 		// Enable async HTTP for performance boost
 		$httpProvider.useApplyAsync(true);
 	})
+	// }}}
 
-	/**
-	* Router behaviours
-	*/
-	.run(function($config, $loader, $rootScope, $router) {
-		// If nothing matches go to '/' {{{
-		$router.rule().priority('lowest').go('/');
-		// }}}
+	// Router config {{{
 
-		// Animate the page loader while navigating {{{
+	// If no route matches go to '/' {{{
+	.run(function($router) {
+		$router.rule().priority('lowest').redirect('/');
+	})
+	// }}}
+
+	// Redirect any page navigation (that is not in an array of approved ones) to /login if the user is not logged in {{{
+	.run(function($location, $rootScope, $session) {
+		$rootScope.$on('$routerStart', function(e, rule) {
+			if (!rule) return; // No route figured out yet
+			// Array of component names that we will allow if the user is not logged in
+			var allowComponents = ['loginCtrl', 'logoutCtrl', 'usersInviteCtrl', 'usersInviteAcceptCtrl'];
+			if (_.includes(allowComponents, rule._component)) return; // Component is already approved - do nothing
+			$session.promise() // Ask session if we are logged in
+				.catch(_=> $location.path('/login')) // Not logged in - redirect to /login
+		});
+	})
+	// }}}
+
+	
+	// Animate the page loader while navigating {{{
+	.run(function($loader, $rootScope) {
 		$rootScope.$on('$routerStart', _=> $loader.clear().start('routerNav'));
 		$rootScope.$on('$routerSuccess', _=> $loader.stop('routerNav'));
 		$rootScope.$on('$routerError', _=> $loader.stop('routerNav'));
-		// }}}
+	})
+	// }}}
 
-		// Cleanup Bootstrap elements on navigation {{{
+	// Cleanup Bootstrap elements on navigation {{{
+	.run(function($rootScope) {
 		$rootScope.$on('$routerStart', _=> {
 			// Destory any open Bootstrap modals
 			$('body > .modal-backdrop').remove();
@@ -33,20 +53,26 @@ angular
 			// Destroy any open Bootstrap popovers
 			$('body > .popover').remove();
 		});
-		// }}}
-
-		// Focus any input element post-navigation {{{
-		$rootScope.$on('$routerSuccess', _=> $('div[ui-view=main]').find('input[autofocus]').focus());
-		// }}}
-
-		// Reattach 'waves' effect on every router reload {{{
-		$rootScope.$on('$routerSuccess', _=> Waves.init());
-		// }}}
-
-		// Adjust page title when the page changes {{{
-		$rootScope.$on('$routerSuccess', (e, rule) => document.title = $config.title + (rule.data && rule.data.title ? ' | ' + rule.data.title : ''));
-		// }}}
 	})
+	// }}}
+
+	// Focus any input element post-navigation {{{
+	.run(function($rootScope) {
+		$rootScope.$on('$routerSuccess', _=> $('div[ui-view=main]').find('input[autofocus]').focus());
+	})
+	// }}}
+
+	// Reattach 'waves' effect on every router reload {{{
+	.run(function($rootScope) {
+		$rootScope.$on('$routerSuccess', _=> Waves.init());
+	})
+	// }}}
+
+	// Adjust page title when the page changes {{{
+	.run(function($config, $rootScope) {
+		$rootScope.$on('$routerSuccess', (e, rule) => document.title = $config.title + (rule.data && rule.data.title ? ' | ' + rule.data.title : ''));
+	})
+	// }}}
 
 	// FIXME: Defunct
 	/**
@@ -72,3 +98,5 @@ angular
 		});
 	})
 	*/
+
+	// }}}
