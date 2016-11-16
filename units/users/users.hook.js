9eb76ae46031e6a0f859f6ed735d78ebb2ea9584
Matt Carter <m@ttcarter.com>
2016-11-16 10:14:27 +1000

    BUGFIX: Users backend path should be a hook which registers after all other controllers have decorated the /api/users namespace

---
+++ units/users/users.hook.js
@@ -0,0 +1,14 @@
+var monoxide = require('monoxide');
+
+app.register('postControllers', function(finish) {
+	app.use('/api/users/:id?', monoxide.express.middleware('users', {
+		query: app.middleware.ensure.admin,
+		create: app.middleware.ensure.admin,
+		save: app.middleware.ensure.admin,
+		get: app.middleware.ensure.login,
+		delete: app.middleware.ensure.admin,
+		meta: app.middleware.ensure.admin,
+	}));
+
+	finish();
+});
