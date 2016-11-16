9d4e4eeb2555b7aef3ee6f8c7049588e8879fc9e
Matt Carter <m@ttcarter.com>
2016-11-16 10:10:10 +1000

    BUGFIX: Fonts now uses correct hook

--- units/fonts/fonts.hook.js
+++ units/fonts/fonts.hook.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-app.register('preServer', function(finish) {
+app.register('postControllers', function(finish) {
 	app.use('/fonts', express.static(app.config.paths.root + '/node_modules/font-awesome/fonts', {extensions: ['eot', 'svg', 'ttf', 'woff', 'woff2']}));
 	app.use('/fonts', express.static(__dirname, {extensions: ['eot', 'svg', 'ttf', 'woff', 'woff2']}));
 
