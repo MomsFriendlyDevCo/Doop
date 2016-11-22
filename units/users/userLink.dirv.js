9469efcbc3cbe4fa77162f849a27262358d1abee
Matt Carter <m@ttcarter.com>
2016-11-23 09:26:43 +1000

    Added user linkage directive

---
+++ units/users/userLink.dirv.js
@@ -0,0 +1,21 @@
+/**
+* Generic widget to display a users details inline
+* This avoids having to do 'does the user have an email' tests each time we want to show a name
+*
+* @example
+* <user-widget user="someUser"></user-widget>
+*/
+angular
+	.module('app')
+	.component('userLink', {
+		bindings: {
+			user: '=',
+		},
+		template: `
+			<a href="#/users/{{$ctrl.user._id}}">
+				<i class="fa" ng-class="{'fa-user-circle': $ctrl.user.role == 'admin' || $ctrl.user.role == 'owner', 'fa-user': $ctrl.user.role == 'staff', 'fa-user-o': $ctrl.user.role == 'client'}"></i>
+				{{$ctrl.user.name}}
+				<span ng-if="$ctrl.user.email">({{$ctrl.user.email}})</span>
+			</a>
+		`,
+	})
