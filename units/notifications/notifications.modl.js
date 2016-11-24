ec20a2437c2c6674acf095ea41a1f412ae782a3a
Matt Carter <m@ttcarter.com>
2016-11-24 13:02:16 +1000

    Added default ReST operations to ngResource (count, create, meta)

--- units/notifications/notifications.modl.js
+++ units/notifications/notifications.modl.js
@@ -2,7 +2,6 @@ angular
 	.module('app')
 	.factory('Notifications', function($resource) {
 		return $resource('/api/notifications/:id',{}, {
-			count: {method: 'GET', url: '/api/notifications/count'},
 			markAsRead: {method: 'GET', url: '/api/notifications/markAsRead'},
 		});
 	});
