78c63e787cbf46791e192bf5ae7628325d8170bb
theServerGod <the.server.god@gmail.com>
2017-03-02 11:01:30 +1000

    Updated server error handler hook to do content-negotiation for response

--- units/errors/errors.hook.js
+++ units/errors/errors.hook.js
@@ -1,16 +1,40 @@
 app.register('postServer', function(finish) {
 	// Handle 404
 	app.use(function(req, res) {
-		res.status(404).render('units/errors/404', {
-			theme: app.config.title,
+		res.format({
+			text: function() {
+				res.status(404).send('Page not found!');
+			},
+
+			html: function() {
+				res.status(404).render('units/errors/404', {
+					theme: app.config.title,
+				});
+			},
+
+			json: function() {
+				res.status(404).send({ error: 'Page not found!' });
+			}
 		});
 	});
 
 	// Handle 500
-	app.use(function(err, req, res, next){
+	app.use(function(err, req, res, next) {
 		console.error(err.stack);
-		res.status(500).render('units/errors/500', {
-			theme: app.config.title,
+		res.format({
+			text: function() {
+				res.status(500).send('Something went wrong!');
+			},
+
+			html: function() {
+				res.status(500).render('units/errors/500', {
+					theme: app.config.title,
+				});
+			},
+
+			json: function() {
+				res.status(500).send({ error: 'Something went wrong!' });
+			}
 		});
 	});
 
