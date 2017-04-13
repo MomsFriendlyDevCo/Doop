0370a206aa1f5dd320e2681c3dff6eedf50ce683
Matt Carter <m@ttcarter.com>
2017-04-13 16:59:23 +1000

    Added ability to get version info via /api/debug/version

--- units/debug/debug.ctrl.js
+++ units/debug/debug.ctrl.js
@@ -3,14 +3,30 @@ angular
 	.run($router => $router.when('/debug').title('Debug').component('debugCtrl'))
 	.component('debugCtrl', {
 		templateUrl: '/units/debug/debug.tmpl.html',
-		controller: function($config, $loader, $router, $session, $toast, $tts) {
+		controller: function($config, $loader, $http, $q, $router, $session, $scope, $toast, $tts, AssetTrees) {
 			var $ctrl = this;
-
 			$ctrl.$config = $config;
 			$ctrl.$loader = $loader;
 			$ctrl.$router = $router;
 			$ctrl.$session = $session;
 			$ctrl.$toast = $toast;
 			$ctrl.$tts = $tts;
+
+			// Data refresher {{{
+			$ctrl.version;
+			$ctrl.refresh = ()=> {
+				$loader.startBackground($scope.$id);
+
+				$q.all([
+					// Fetch version info
+					$http.get('/api/debug/version')
+						.then(res => $ctrl.version = res.data),
+				])
+					.catch($toast.catch)
+					.finally($loader.stop($scope.$id));
+			};
+			// }}}
+
+			$scope.$evalAsync($ctrl.refresh);
 		},
 	});
--- units/debug/debug.path.js
+++ units/debug/debug.path.js
@@ -1,4 +1,7 @@
+var async = require('async-chainable');
+var asyncExec = require('async-chainable-exec');
 var email = require('mfdc-email');
+var fs = require('fs');
 
 /**
 * Test email sending
@@ -18,4 +21,55 @@ app.get('/api/debug/test/email', app.middleware.ensure.root, function(req, res) 
 				response: emailRes,
 			});
 		})
+});
+
+
+/**
+* Fetch Git information
+* NOTE: This will only execute ONCE per execution cycle to prevent DDOS
+*/
+var _cachedVersion;
+app.get('/api/debug/version', function(req, res) {
+	if (_cachedVersion) return res.send(_cachedVersion);
+
+	async()
+		.parallel({
+			git: function(next) {
+				async()
+					.use(asyncExec)
+					.exec('git', ['git', 'log', '-1', `--pretty=format:%H|%h|%cI|%cn|%s`])
+					.then('git', function(next) {
+						var gitInfo = this.git[0].split('|');
+						next(null, {
+							hash: gitInfo[0],
+							shortHash: gitInfo[1],
+							date: gitInfo[2],
+							committer: gitInfo[3],
+							subject: gitInfo[4],
+						});
+					})
+					.end(function(err) {
+						if (err) return next(err);
+						next(null, this.git);
+					});
+			},
+			package: function(next) {
+				var packagePath = app.config.paths.root + '/package.json';
+				fs.stat(packagePath, function(err, stat) {
+					if (err) return next(err);
+					next(null, {
+						version: require(packagePath).version,
+						updated: stat.mtime,
+					});
+				});
+			},
+		})
+		.end(function(err) {
+			if (err) return res.status(400).send(err.toString()).end();
+			_cachedVersion = {
+				package: this.package,
+				git: this.git,
+			};
+			res.send(_cachedVersion);
+		});
 });
