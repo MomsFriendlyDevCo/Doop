3317b233a725f0ed4909cc8e07ed9d9bc6fc24b4
Matt Carter <m@ttcarter.com>
2017-12-29 15:10:16 +1000

    Added linting gulp task

---
+++ gulp/lint.gulp.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var replace = require('gulp-replace');
+
+gulp.task('lint:autofix', ['lint:autofix:css', 'lint:autofix:js', 'lint:autofix:html']);
+
+gulp.task('lint:autofix:js', ['load:app'], function() {
+	return gulp.src(`${paths.root}/units/**/*.js`)
+		.pipe(replace(/\s+$/g, '')) // Right-Trim whitespace at end of lines
+		.pipe(replace(/\(\)\s*=>\s*{/g, '()=> {')) // e.g. `()=>{...` -> `() => {...`
+		.pipe(replace(/([a-z0-9]+)\s*=>\s*/g, '$1 => ')) // e.g. `var=>` -> `var => `
+		.pipe(replace(/if\(/g, 'if ('))
+		.pipe(replace(/if\s*\((.+?)\)\s*{/g, 'if ($1) {'))
+		.pipe(gulp.dest(`${paths.root}/units`))
+});
+
+gulp.task('lint:autofix:css', ['load:app'], function() {
+	return gulp.src(`${paths.root}/units/**/*.css`)
+		.pipe(replace(/\s+$/g, ''))
+		.pipe(gulp.dest(`${paths.root}/units`))
+});
+
+gulp.task('lint:autofix:html', ['load:app'], function() {
+	return gulp.src(`${paths.root}/units/**/*.html`)
+		.pipe(replace(/\s+$/g, ''))
+		.pipe(gulp.dest(`${paths.root}/units`))
+});
