25e437e677d87cf94b0705999db7669ac5c70c3f
Matt Carter <m@ttcarter.com>
2018-04-11 13:29:27 +1000

    Site layout now uses sitemap unit rather than static HTML sidebar

---
+++ units/sitemap/sitemap.serv.js
@@ -0,0 +1,225 @@
+angular
+	.module('app')
+	.service('$sitemap', function($injector, $q, $rootScope, $router, $session, TreeTools) {
+		var $sitemap = this;
+
+		// Sidebar links & structure {{{
+		/**
+		* Each collection item is defined as:
+		*
+		* NOTE: This is the un-calculated raw data, for the flattened, evaluated data use $sitemap.map
+		*
+		* @var {Object}
+		* @param {string} title The human readable title of each sidebar item
+		* @param {string|function} href The link to redirect to (include '#/' if an Angular resource). If this is a function it is evaluated using the Angular $injector.invoke() service and is expected to return a string.
+		* @param {string} icon The icon class to display next to each item
+		* @param {array} children Any child items, the definition of each is the same as the main element. If this is a function it is evaluated using the Angular $injector.invoke() service and is expected to return a Promise.
+		* @param {function} show Function evaluated as ($session) to determine if the items should be shown. Parent items will hide all child items if the parent is not shown
+		* @see $sitemap.map
+		*/
+		$sitemap.links = [
+			{
+				title: 'Dashboard',
+				href: '#/',
+				icon: 'fa fa-fw fa-dashboard',
+			},
+			{
+				title: 'Notifications',
+				href: '#/notifications',
+				icon: 'fa fa-fw fa-envelope',
+			},
+			{
+				title: 'Admin',
+				href: '#/admin',
+				icon: 'fa fa-fw fa-user',
+				show: $session => $session.data.isAdmin,
+				// Sub-items {{{
+				children: [
+					{
+						title: 'Users',
+						href: '#/admin/users',
+					},
+				],
+				// }}}
+			},
+			{
+				title: 'Debugging',
+				href: '#/debug',
+				icon: 'fa fa-fw fa-heartbeat',
+				show: $session => $session.data.isAdmin || $session.data.permissions.debugging,
+				// Sub-items {{{
+				children: [
+					{
+						title: 'Server',
+						href: '#/debug/server',
+						// Sub-items {{{
+						children: [
+							{
+								title: 'Git state',
+								href: '#/debug/server/git',
+							},
+							{
+								title: 'Server info',
+								href: '#/debug/server/info',
+							},
+						],
+						// }}}
+					},
+					{
+						title: 'Services',
+						href: '#/debug/services',
+						// Sub-items {{{
+						children: [
+							{
+								title: '$config',
+								href: '#/debug/services/config',
+							},
+							{
+								title: '$http',
+								href: '#/debug/services/http',
+							},
+							{
+								title: '$loader',
+								href: '#/debug/services/loader',
+							},
+							{
+								title: '$prompt',
+								href: '#/debug/services/prompt',
+							},
+							{
+								title: '$screen',
+								href: '#/debug/services/screen',
+							},
+							{
+								title: '$session',
+								href: '#/debug/services/session',
+							},
+							{
+								title: '$toast',
+								href: '#/debug/services/toast',
+							},
+							{
+								title: '$tts',
+								href: '#/debug/services/tts',
+							},
+						],
+						// }}}
+					},
+					{
+						title: 'Rockjaw Specific',
+						href: '#/debug/rockjaw',
+						// Sub-items {{{
+						children: [
+							{
+								title: 'Pending',
+								href: '#/debug/rockjaw/pending',
+							},
+							{
+								title: 'Pickers',
+								href: '#/debug/rockjaw/pickers',
+							},
+						],
+						// }}}
+					},
+				],
+				// }}}
+			},
+		];
+		// }}}
+
+		// Data refresher (main map) {{{
+		/**
+		* Create the $sitemap.map object from $sitemap.links
+		* Also ensures that that each item has a link and functions are flattened
+		* @returns {Promise} Resolves with the built sitemap
+		*/
+		$sitemap.map;
+		$sitemap.refresh = ()=>
+			$q.resolve()
+				.then(()=> $session.promise()) // Ensure the user profile pull has completed first
+				.then(() => {
+					$sitemap.map = angular.copy($sitemap.links);
+					$sitemap.decorateNodes($sitemap.map);
+					return $sitemap.map;
+				})
+
+		/**
+		* Calls refresh() if sitemap doesn't already exist, otherwise returns it
+		* This can be used to force the sitemap to build before any downstream promise requires it
+		* @returns {Promise} Resolves with the built sitemap
+		*/
+		$sitemap.get = ()=> $q(resolve => {
+			if ($sitemap.map) {
+				resolve($sitemap.map);
+			} else {
+				return $sitemap.refresh().then(map => resolve(map));
+			}
+		});
+		// }}}
+
+		// Data refresher (active items) {{{
+		$rootScope.$on('$routerSuccess', ()=> {
+			if (!$sitemap.map) return; // Not yet ready
+
+			// Set all nodes to inactive
+			TreeTools.flatten($sitemap.map)
+				.forEach(node => node.active = false);
+
+			// Bottom-up search through the tree, marking each node as active
+			var selectedCount = 0;
+			TreeTools.parents($sitemap.map, item => item.href && $router.path == item.href.replace(/^#/, '').replace(/\?.*$/, ''))
+				.forEach(node => {
+					node.active = true;
+					selectedCount++;
+				});
+
+			// Nothing was seleted? Try the best guess
+			if (!selectedCount) {
+				var bestGuess = _(TreeTools.flatten($sitemap.map))
+					.filter(item => item.href && item.href + '/' == '#' + $router.path.substr(0, item.href.length))
+					.map(item => _.set(item, 'length', item.href.length))
+					.sortBy('length')
+					.reverse()
+					.first();
+
+				if (bestGuess) {
+					TreeTools.parents($sitemap.map, {id: bestGuess.id})
+						.forEach(node => node.active = true);
+				} else {
+					console.warn('Sitemap - Cannot find location within tree', 'Router path:', $router.path);
+				}
+			}
+		});
+		// }}}
+
+		// Utility functions {{{
+		/**
+		* Iterate over a tree of items and decorate each item
+		* NOTE: This mutates nodes
+		* @param {Object|array} nodes The nodes to decorate
+		*/
+		$sitemap.decorateNodes = nodes => {
+			TreeTools.flatten(nodes).forEach(node => {
+				// Calculate .show
+				node.show = node.show ? node.show($session) : true;
+
+				// Flatten href
+				if (angular.isFunction(node.href)) node.href = $injector.invoke(node.href);
+
+				// Flatten children
+				if (angular.isFunction(node.children)) $injector.invoke(node.children).then(data => {
+					$sitemap.decorateNodes(data);
+					node.children = data;
+				});
+
+				// Add .id if missing
+				if (!node.id) node.id = (node.href || node.title).replace(/[^a-z0-9]+/ig, '');
+			});
+		};
+		// }}}
+
+		// React to user permission changes
+		$rootScope.$on('session.updated', ()=> $sitemap.refresh());
+
+		$sitemap.refresh();
+	})
--- units/theme.sidebar/sidebar.dirv.js
+++ units/theme.sidebar/sidebar.dirv.js
@@ -2,19 +2,22 @@ angular
 	.module('app')
 	.component('sidebar', {
 		templateUrl: '/units/theme.sidebar/sidebar.tmpl.html',
-		controller: function($element, $rootScope, $router, $session) {
+		controller: function($scope, $sitemap, $element, $rootScope, $router, $screen, $session, $window) {
 			var $ctrl = this;
 			$ctrl.$session = $session;
-			$ctrl.$router = $router;
+			$ctrl.$sitemap = $sitemap;
 
+			$ctrl.searchTerm = '';
 			$ctrl.areas; // Array of each section of the route (seperated by -; e.g. '#/foo/bar' => [foo,bar])
+			$ctrl.area; // First segment of $scope.areas
+			$ctrl.query;
 
-			$ctrl.setArea = function() {
-				if (!$ctrl.$router.path) return; // Router not ready yet
-				$ctrl.areas = $ctrl.$router.path.split('/').slice(1); // Split into path segments (remove first empty element)
-			}
-
-			$rootScope.$on('$routerSuccess', ()=> $ctrl.setArea());
+			$rootScope.$on('$routerSuccess', ()=> {
+				if (!$router.path) return; // Router not ready yet
+				$ctrl.areas = $router.path.split('/').slice(1); // Split into path segments (remove first empty element)
+				$ctrl.area = this.areas[0];
+				$ctrl.query = $router.query;
+			});
 
 			$('.sidebar-toggle').click(()=> {
 				$('body').toggleClass('sidebar-toggled');
@@ -23,11 +26,30 @@ angular
 
 			// Close sidebar when in mobile mode on any click event
 			$element.on('click', 'a', function() {
+				if (!$screen.isMobile) return; // Don't bother if we're not mobile
 				if ($(this).data('toggle') == 'collapse') return; // Ignore menus that collapse
-				$('body').removeClass('sidebar-toggled');
-				$('.hi-trigger, #sidebar').removeClass('toggled');
+				$('#container').removeClass('mainnav-in').addClass('mainnav-sm');
+				$('.navbar-brand img.brand-icon').attr('src', $('.navbar-brand img.brand-icon').data('src-sm'));
 			});
 
-			$ctrl.setArea();
+			// Toggle the size of the sidebar when a.mainnav-toggle gets clicked {{{
+			$('.mainnav-toggle').click(function() {
+				// Starting base - this occurs on page refresh.
+				if (!$('#container').hasClass('mainnav-sm') && !$('#container').hasClass('mainnav-in')) {
+					// Phone and tablet size screens
+					if ($(window).width() < 768) $('#container').addClass('mainnav-sm');
+				}
+
+				if ($('#container').hasClass('mainnav-sm')) {
+					// Large mode
+					$('#container').addClass('mainnav-in').removeClass('mainnav-sm');
+					$('.navbar-brand img.brand-icon').attr('src', $('.navbar-brand img.brand-icon').data('src-lg'));
+				} else {
+					// Small mode
+					$('#container').addClass('mainnav-sm').removeClass('mainnav-in');
+					$('.navbar-brand img.brand-icon').attr('src', $('.navbar-brand img.brand-icon').data('src-sm'));
+				}
+			});
+			// }}}
 		},
-	});
+	})
