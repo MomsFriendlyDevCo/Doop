851b71d6771286faf3e62408dc6c8fccda6a45bc
Matt Carter <m@ttcarter.com>
2019-08-20 15:17:09 +1000

    BUGFIX: Waves effect only bound when DOM was ready

--- skeleton/vendors/waves.vue
+++ skeleton/vendors/waves.vue
@@ -7,14 +7,18 @@
 * Inject the material design waves effect to all things that look like buttons
 * Requires 'node-waves' NPM
 */
-app.ready.then(()=> window.app.router.afterEach((to, from) => {
-	// NOTE: All the below attachment rules assumes `waves-effect` is applied anyway
-	$('.btn').addClass('waves-effect');
-	$('.btn-circle, .btn-ellipsis').addClass('waves-circle');
-	$('.input-group > .btn').addClass('waves-table-cell');
-	$('a.list-group-items, .dropdown-menu > li > a, .main-menu > li > a').addClass('waves-effect waves-block');
+app.ready.then(()=>
+	app.router.afterEach((to, from) =>
+		Vue.nextTick(()=> {
+			// NOTE: All the below attachment rules assumes `waves-effect` is applied anyway
+			$('.btn').addClass('waves-effect');
+			$('.btn-circle, .btn-ellipsis').addClass('waves-circle');
+			$('.input-group > .btn').addClass('waves-table-cell');
+			$('a.list-group-item, .dropdown-menu > li > a, .main-menu > li > a').addClass('waves-effect waves-block');
 
-	// Re-init
-	Waves.init();
-}));
+			// Re-init
+			Waves.init();
+		})
+	)
+);
 </script>
