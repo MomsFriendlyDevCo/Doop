fb20b7c13325ad1897cd36c4a23e57cdca844cd6
Matt Carter <m@ttcarter.com>
2019-09-20 16:47:01 +1000

    Added pluralization filter

---
+++ skeleton/filters/plural.vue
@@ -0,0 +1,52 @@
+<filter>
+/**
+* Pluralise various words based on the numeric prefix
+* This function is a fancy find/replace system which examines numbers before words and tries to pluralise them
+*
+* NOTE: Auto pluralisation is pretty stupid and really just adds 's' as a suffix for plurals or removes it for singulars unless explicitally specified
+* NOTE: Input numbers can also be formatted (e.g. '1,223,123'), only '1' is really checked for
+*
+* @param {array|string} plurals Expressions to force search for either as an array or CSV, Each entity can be seperated by slashes to force definitions e.g. `['widget/widgets']`
+* 
+* @example Pluralise automatically
+* {{'1 widget' | plural}} //= '1 widget' / '2 widgets'
+*
+* @example Pluralise with definitions
+* {{'1 person' | plural('person/people')}} //= '1 person' / '1 people'
+*/
+module.exports = (value, plurals) => {
+	var singularToPlural = {};
+	var pluralToSingular = {};
+
+	// Calculate singular lookups {{{
+	if (plurals) { // Digest plural definitions
+		if (typeof plurals == 'string') plurals = plurals.split(/\s*,\s*/); // Split from CSV
+		plurals.forEach(p => {
+			var [singular, plural] = p.split(/\s*\/\s*/);
+			if (!singular || !plural) throw new Error(`Invalid plural spec definition "${p}", must be of format "singular / plural"`);
+			singularToPlural[singular] = plural;
+			pluralToSingular[plural] = singular;
+		});
+	}
+	// }}}
+
+	return value.replace(/([\d,]+) ([a-z]+)\b/gi, (match, numberRaw, wordRaw) => {
+		// Widgets (P2S, clip 's'), Widget (S2P -> P2S
+		var wordSingular =
+			pluralToSingular[wordRaw]
+			|| singularToPlural[wordRaw] ? pluralToSingular[singularToPlural[wordRaw]] : null
+			|| wordRaw.replace(/s$/, '');
+
+		var wordPlural =
+			pluralToSingular[wordRaw] ? singularToPlural[pluralToSingular[wordRaw]] : null
+			|| singularToPlural[wordRaw]
+			|| wordRaw.endsWith('s') ? wordRaw : wordRaw + 's';
+
+		if (numberRaw == '1') { // Use singular
+			return numberRaw + ' ' + wordSingular;
+		} else { // Use multiple plural
+			return numberRaw + ' ' + wordPlural;
+		}
+	});
+};
+</filter>
