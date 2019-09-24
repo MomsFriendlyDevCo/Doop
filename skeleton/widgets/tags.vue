b24df097e28a03b7f2a6bd10e2aa76e4ffceba8a
Matt Carter <m@ttcarter.com>
2019-09-24 16:19:38 +1000

    Added tag widget

---
+++ skeleton/widgets/tags.vue
@@ -0,0 +1,83 @@
+<component>
+/**
+* Tag selection widget obeying an opinionated internal spec
+*
+* FIXME: This widget does not yet support adding new tags, only selecting from existing ones within the spec
+* FIXME: This widget does not yet apply color classing to items
+*
+* @param {array <Object>} value The current array of selected tags
+* @param {string} value.tag The tag string (corresponds with spec.tag)
+* @param {date} [value.created] When the tag was created, inherits from the current date for new tags
+* @param {string} [value.creator] Who created the tag was created, inherits from $session.data._id for new tags
+*
+* @param {array <Object>} spec The specification of tags the user can select
+* @param {string} spec.tag The value of the tag, must be a-z or hyphen in lower case
+* @param {string} [spec.color="var(--secondary)"] The color of the tag to display
+* @param {string} [spec.group] The item group, forms a mutual-exclusion with other tags, tagging cannot have two elements of the same group and placing a new group item removes the other
+* @param {date} [spec.created] When the spec item was created
+* @param {string} [spec.creator] The user ID of the user who created the tag spec
+*
+* @param {boolean} [disabled=false] Whether to disallow editing
+* @param {string} [placeholder] Placeholder to display when no tags are selected
+* 
+* @emits change Emitted as `(tags)` when the tag selection changes
+*/
+module.exports = {
+	props: {
+		value: Array,
+		spec: Array,
+		disabled: {type: Boolean, default: false},
+		placeholder: String,
+	},
+	computed: {
+		options() {
+			return (this.spec || []).map(s => ({
+				color: 'var(--secondary)',
+				tag: s.tag,
+				value: s.tag,
+			}));
+		},
+	},
+	methods: {
+		changed(val) {
+			// Deduplicate groups {{{
+			val = _(val)
+				.groupBy(v => {
+					var tagMeta = this.$props.spec.find(s => s.tag == v.tag);
+					if (!tagMeta) throw new Error(`Tag "${v.tag}" is a member of no item in spec!`);
+					return tagMeta.group;
+				})
+				.tap(v => console.log('Computed groups', v))
+				.mapValues((members, group) => {
+					if (!group) return members; // Ignore non-grouped items
+					if (members.length == 1) return members; // Ignore single item groups
+
+					return _.last(members); // Take last entry as its likely to be the one the user is replacing existing values with
+				})
+				.values()
+				.flatten()
+				.value()
+			// }}}
+
+			this.$emit('change', val.map(v => ({
+				tag: v.tag,
+				created: v.created || new Date(),
+				creator: v.creator || this.$session.data._id,
+			})));
+		},
+	},
+};
+</component>
+
+<template>
+	<v-select
+		multiple
+		:value="$props.value"
+		:options="options"
+		:select-on-tab="true"
+		:disabled="$props.disabled"
+		:placeholder="$props.placeholder"
+		label="tag"
+		@input="changed"
+	/>
+</template>
