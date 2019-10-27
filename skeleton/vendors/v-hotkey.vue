99d1ccae881bbdcd7987cc586f8a18f83441638d
Matt Carter <m@ttcarter.com>
2019-10-27 12:30:15 +0000

    Added v-hotkey directive to standard project layout

---
+++ skeleton/vendors/v-hotkey.vue
@@ -0,0 +1,11 @@
+<script repack>
+/**
+* Importer for the v-hotkey component
+* @url https://github.com/Dafrok/v-hotkey
+*
+* @example
+* <div v-hotkey="{'ctrl+a': selectAll}"/>
+*/
+import VueHotkey from 'v-hotkey'
+Vue.use(VueHotkey);
+</script>
