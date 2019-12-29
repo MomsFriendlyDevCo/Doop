a29b75a160d1f925d8c10bcc3b6b0c255652a172
Matt Carter <m@ttcarter.com>
2019-12-29 10:06:51 +1000

    Added $timeout service

---
+++ skeleton/services/timeout.vue
@@ -0,0 +1,14 @@
+<service singleton>
+/**
+* Acts like a combination of Vue.$nextTick + setTimeout() to wait until Vue has finished its processing
+* @param {function} func Function to run when ready
+* @param {number} [timeout=100] Delay in MS to wait before running the function
+*/
+module.exports = function() {
+	var $timeout = function(func, timeout) {
+		Vue.nextTick(()=> setTimeout(func, timeout));
+	};
+
+	return $timeout;
+};
+</service>
