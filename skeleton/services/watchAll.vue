f8e584016b5fff1e5e2a20a0c30220fe960aeac3
Matt Carter <m@ttcarter.com>
2020-01-08 15:39:21 +1000

    $watchAll now supports `{immediate: true}` option BUGFIX: $watchAll doesnt get stuck in loops when processing callbacks BUGFIX: $watchAll now supports locking when all values are ready, defaults to true

--- skeleton/services/watchAll.vue
+++ skeleton/services/watchAll.vue
@@ -2,27 +2,38 @@
 /**
 * Binds a simple vm.$watchAll([props...], handler) function into the prototype scope
 * This works similar to the Angular vm.$watch([...]) function
-* @param {array <string>} props Named properties within `vm` to watch
+* @param {array <string|array>} props Named properties within `vm` to watch, dotted notation or array notation is supported
 * @param {function} callback The callback to run when the watch triggers
 * @param {Object} [options] Additional options
+* @param {boolean} [options.immediate=false] Execute the callback immediately on register, needAll can cause the callback not to fire if all required paths are not available yet
 * @param {boolean} [options.needAll=false] Only fire the watcher when every single member listed has a non-undefined value
+* @param {boolean} [options.needAllLock=true] WHether, after seeing all paths, we ALWAYS route to the callback in future, even if the values become undefined. Set to falsy to always check for the paths presence
 * @param {boolean} [options.preventLoops=true] Assume that the watcher can effect a peer expression and that we should not keep retriggering this watch expression
 */
 module.exports = function(props, callback, options) {
 	var settings = {
 		needAll: false,
+		needAllLock: true,
 		preventLoops: true,
+		immediate: false,
 		...options,
 	};
 
 	// Remap handler based on the settings {{{
 	var handler;
 	if (settings.needAll) { // Remap handler with extra behaviour that checks all properties first
+		var seenAll = false; // Whether at some point we have seen all values
 		handler = ()=> {
+			if (settings.needAllLock && seenAll) return callback.apply(this, arguments);
 			if (settings.preventLoops && this.$duringWatchAll) return; // Already updating
 			if (settings.preventLoops) this.$duringWatchAll = true;
-			if (props.some(p => this[p] === undefined)) return; // Not ready yet
-			handler.apply(this, arguments);
+			if (props.some(p => !this.$hasPath(p))) { // Not ready yet
+				this.$duringWatchAll = false;
+				return;
+			} else {
+				seenAll = true;
+			}
+			callback.apply(this, arguments);
 			if (settings.preventLoops) this.$duringWatchAll = false;
 		};
 	} else { // No special treatment - just pass the callback
@@ -31,6 +42,6 @@ module.exports = function(props, callback, options) {
 	// }}}
 		
 	// Attach watcher to every named prop
-	props.forEach(prop => this.$watch(prop, handler.bind(null, prop)));
+	props.forEach(prop => this.$watch(prop, handler.bind(null, prop, {immediate: settings.immediate})));
 }
 </service>
