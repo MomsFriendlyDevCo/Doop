f9e1512f44bedd0bc4ddb213c3a57362dbdc3e61
Matt Carter <m@ttcarter.com>
2020-01-12 17:28:09 +1000

    New compile process - uses @mfdc/crash for reporting, dumps to .error and no longer spews weird garbage with Babel parse errors

--- skeleton/build/build.vue.gulp.js
+++ skeleton/build/build.vue.gulp.js
@@ -4,10 +4,10 @@
 */
 
 var _ = require('lodash');
-var cache = require('gulp-cache');
 var babel = require('gulp-babel');
 var blockHead = require('gulp-block-head');
 var concat = require('gulp-concat');
+var crash = require('@momsfriendlydevco/crash');
 var fs = require('fs');
 var fspath = require('path');
 var gulp = require('gulp');
@@ -25,13 +25,21 @@ gulp.task('build.vue', ['load:app'], ()=>
 		'!dist/**/*',
 		'!node_modules/**/*',
 	])
+		// .error file generation on crash (or cleanup if everything ok) {{{
+		.on('finish', ()=> fs.promises.unlink(`${__dirname}/.error`).catch(()=> null)) // Remove .error file on successful compile
 		.pipe(plumber({
 			errorHandler: err => {
-				gutil.log(gutil.colors.red('ERROR DURING JS BUILD'));
-				process.stdout.write(err.stack);
-				console.log('EXIT ERROR', err);
+				var trace = crash.generate(err, {
+					prefix: 'ERROR DURING VUE BUILD',
+					context: {
+						pathRewrite: path => path.replace(/#(.*)$/, ''), // Transform `thing.js#service` -> `thing.js`
+					},
+				});
+				process.stderr.write(trace);
+				return fs.promises.writeFile(`${app.config.paths.root}/.error`, trace);
 			},
 		}))
+		// }}}
 		.pipe(gulpIf(app.config.gulp.debugJS, sourcemaps.init()))
 		.pipe(replace(/0\/\* @IMPORT: (.+?) \*\//g, (junk, i) => JSON.stringify(_.get(global, i)))) // Import variables via `0/* @IMPORT: app.path */`
 		.pipe(blockHead({
@@ -43,7 +51,6 @@ gulp.task('build.vue', ['load:app'], ()=>
 			blocks: { // Listed in insertation order
 				script: {
 					sort: 0,
-					name: path => `scripts/${fspath.basename(path, '.vue')}.js`,
 					// Only parse script blocks without `repack` attribute as they are handled by vendors/build.repack.gulp.js
 					transform: (content, path, block) => block.attr.repack ? '' : content,
 				},
@@ -52,19 +59,16 @@ gulp.task('build.vue', ['load:app'], ()=>
 				},
 				filter: {
 					sort: 2,
-					name: path => `filters/${fspath.basename(path, '.vue')}.js`,
 					transform: (content, path, block) =>
 						`Vue.assets.filter('${_.camelCase(block.attr.name || fspath.basename(path, '.vue'))}', ${_.trimEnd(content, ';')});`,
 				},
 				directive: {
 					sort: 3,
-					name: path => `directives/${fspath.basename(path, '.vue')}.js`,
 					transform: (content, path, block) =>
 						`Vue.assets.directive('${_.camelCase(block.attr.name || fspath.basename(path, '.vue'))}', ${_.trimEnd(content, ';')});`,
 				},
 				service: {
 					sort: 4,
-					name: path => `services/${fspath.basename(path, '.vue')}.js`,
 					transform: (content, path, block) => // Pass all attrs as arguments to the Vue.assets.service(id, ..., [options]) handler
 						_.isEmpty(block.attr)
 						? `Vue.assets.service('$${_.camelCase(block.attr.name || fspath.basename(path, '.vue'))}', ${_.trimEnd(content, ';')});`
@@ -72,7 +76,6 @@ gulp.task('build.vue', ['load:app'], ()=>
 				},
 				template: {
 					sort: 5,
-					name: (path, block) => `templates/${_.camelCase(block.attr.name || fspath.basename(path, '.vue'))}.js`,
 					transform: (content, path, block) => `Vue.assets.template('${_.camelCase(block.attr.name || fspath.basename(path, '.vue'))}', '`
 						+ _.trimEnd(content, ';')
 							.replace(/\n/g, '\\n')
@@ -82,13 +85,11 @@ gulp.task('build.vue', ['load:app'], ()=>
 				},
 				macgyver: {
 					sort: 9, // Add after templates
-					name: path => `macgyver/${fspath.basename(path, '.vue')}.js`,
 					transform: (content, path) =>
 						`Vue.assets.macgyver('${fspath.basename(path, '.vue')}', ${_.trimEnd(content, ';')});`,
 				},
 				component: {
 					sort: 9, // Add after templates
-					name: (path, block) => `components/${_.camelCase(block.attr.name || fspath.basename(path, '.vue'))}.js`,
 					transform: (content, path, block) =>
 						`Vue.assets.component('${_.camelCase(block.attr.name || fspath.basename(path, '.vue'))}', `
 						+ _.trimEnd(content, ';')
