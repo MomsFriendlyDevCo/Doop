3a67050ec3cf51d86d5eea80cc5d181b7877458c
Matt Carter <m@ttcarter.com>
2020-01-20 11:37:17 +1000

    Added v-mask directive

---
+++ skeleton/vendors/v-mask.vue
@@ -0,0 +1,17 @@
+<script repack>
+/**
+* Importer for the v-mask directive
+* @url https://github.com/probil/v-mask
+*
+* @example Mask a time + date input
+* <div v-mask="'##/##/#### ##:##'"/>
+*
+* @example Mask credit card input
+* <div v-mask="'#### #### #### ####'"/>
+*
+* @example Mask a number with a thousands seperator
+* <div v-mask="'#,###'"/>
+*/
+import VueMask from 'v-mask'
+Vue.use(VueMask);
+</script>
