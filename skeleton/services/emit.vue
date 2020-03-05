5d9474f14a00de8ebbf6fb85ca6d32452c9e72c4
Matt Carter <m@ttcarter.com>
2020-03-06 09:43:56 +1000

    REFCATOR: $emit service now glues onto search service to avoid context passing issue with sub-services like $emit.up()

--- skeleton/services/emit.vue
+++ skeleton/services/emit.vue
@@ -1,4 +1,6 @@
 <script>
+var methods = {}; // Method's to glue onto vm.$emit
+
 /**
 * Extend the base Vue.prototype.$emit with Vue.prototype.$emit.broadcast()
 * This is effectively the same as an $emit.down from the root node
@@ -6,7 +8,7 @@
 * @param {string} msg The name of the event to emit
 * @param {*} [payload...] The payload of the event
 */
-Vue.prototype.$emit.broadcast = (msg, ...payload) => {
+methods.broadcast = (msg, ...payload) => {
 	app.vue.$emit.call(app.vue, msg, ...payload);
 	app.vue.$emit.down.call(app.vue, msg, ...payload);
 };
@@ -18,7 +20,7 @@ Vue.prototype.$emit.broadcast = (msg, ...payload) => {
 * @param {string} msg The name of the event to emit
 * @param {*} [payload...] The payload of the event
 */
-Vue.prototype.$emit.down = function(msg, ...payload) {
+methods.down = function(msg, ...payload) {
 	this.$children.forEach(child => {
 		child.$emit.call(child, msg, ...payload);
 		child.$emit.down.call(child, msg, ...payload);
@@ -27,12 +29,12 @@ Vue.prototype.$emit.down = function(msg, ...payload) {
 
 
 /**
-* Extend the base Vue.prototype.$emit with Vue.prototype.$emit.up() which recurses though all parents emitting a message
+* Extend the base methods.$emit with Vue.prototype.$emit.up() which recurses though all parents emitting a message
 * NOTE: This does not include the current component level, only the parents
 * @param {string} msg The name of the event to emit
 * @param {*} [payload...] The payload of the event
 */
-Vue.prototype.$emit.up = function(msg, ...payload) {
+methods.up = function(msg, ...payload) {
 	if (!this || !this.$parent) throw new Error('Unable to $emit.up() on non Vue component');
 	var node = this;
 	while (node.$parent) {
@@ -50,7 +52,7 @@ Vue.prototype.$emit.up = function(msg, ...payload) {
 * @param {*} [payload...] The payload of the event
 * @returns {Promise} A promise which will resolve with the Promise.all result of all listeners
 */
-Vue.prototype.$emitPromise = function(msg, ...payload) {
+methods.promise = function(msg, ...payload) {
 	var node = this;
 	if (!node || !this._isVue) throw new Error('Unable to $emit.promise() on non Vue component');
 	if (!node._events[msg] || !this._events[msg].length) return Promise.resolve(); // No listeners anyway
@@ -63,4 +65,33 @@ Vue.prototype.$emitPromise = function(msg, ...payload) {
 		)
 	).then(responses => responses[0]);
 };
+
+
+/**
+* Return if anything is listening to a given event
+* This is useful if the event broadcast would involve some non-trivial processing and it would be beneficial to skip if nothing is listening anyway
+* @param {string} msg The name of the event to emit
+*/
+methods.hasListeners = function(msg) {
+	if (!this || !this.$parent) throw new Error('Unable to $emitHasSubscribers() on non Vue component');
+	return (
+		this._events[msg]
+		&& this._events[msg].length > 0
+	);
+};
+
+Vue.mixin({
+	beforeCreate() {
+		this.$emit = this.$emit.bind(this); // Rebind $emit to this vm so we get the right context
+
+		// Long-winded version of _.mapValues() where we remap each method to a binding of this vm
+		Object.assign(this.$emit, Object.fromEntries(
+			Object.entries(methods).map(i =>
+				[i[0], i[1].bind(this)]
+			)
+		));
+	},
+});
+
+export default methods;
 </script>
