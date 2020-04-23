7f8564d632a9bb6787799058a6bb33da4d231ee7
Matt Carter <m@ttcarter.com>
2020-04-23 15:37:31 +1000

    UX: Better modal autofocus behaviour

--- skeleton/vendors/bs4.vue
+++ skeleton/vendors/bs4.vue
@@ -4,8 +4,18 @@
 */
 app.ready.then(()=> {
 	// When a modal gets shown check if there are any input[autofocus] elements we need to focus
+	// If nothing is marked with [autofocus] the first (sane) input element is used instead
 	$(document).on('shown.bs.modal', ()=> {
-		$('.modal.shown input[autofocus]').focus();
+		var autoFocusable = $('.modal.show input[autofocus]')
+
+		if (autoFocusable.length) { // Found something to focus
+			autoFocusable.first().focus();
+		} else { // No auto focus items - find first input (if any) and use that
+			var cand = $('.modal.show input[type]')
+				.filter((i, el) => ['text', 'email', 'number', 'url'].includes($(el).attr('type')))
+				.first()
+				cand.focus();
+		}
 	});
 });
 </script>
