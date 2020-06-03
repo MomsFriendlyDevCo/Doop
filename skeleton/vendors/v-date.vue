2e8354fbf2a4f7aa9afe829f58aa8c43959ee8d7
Matt Carter <m@ttcarter.com>
2020-06-03 13:39:07 +1000

    NPM: Added v-date (a non painful date picker)

---
+++ skeleton/vendors/v-date.vue
@@ -0,0 +1,14 @@
+<script repack>
+/**
+* Import the vuejs-datepicker as v-date
+* @url https://www.npmjs.com/package/vuejs-datepicker
+*
+* @example Simple date binding
+* <v-date v-model="myDate"/>
+*
+* @example Expanded binding
+* <v-date :value="myDate" @selected="myDate = $event"/>
+*/
+import Datepicker from 'vuejs-datepicker';
+Vue.component('v-date', Datepicker);
+</script>
