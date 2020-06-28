64d206918ecbf919f99f84562b75c7b5178519c1
Matt Carter <m@ttcarter.com>
2020-06-28 14:36:20 +1000

    UX: More options when setting autofocus properties within modals

--- skeleton/vendors/bs4.vue
+++ skeleton/vendors/bs4.vue
@@ -5,11 +5,16 @@
 app.ready.then(()=> {
 	// When a modal gets shown check if there are any input[autofocus] elements we need to focus
 	// If nothing is marked with [autofocus] the first (sane) input element is used instead
+	// Additionally if we find the 'data-autofocus-method' attribute this indicates how we should handle focusing (values are 'focus' (default) and 'select')
 	$(document).on('shown.bs.modal', ()=> {
 		var autoFocusable = $('.modal.show input[autofocus]')
 
 		if (autoFocusable.length) { // Found something to focus
-			autoFocusable.first().focus();
+			if (autoFocusable.data('autofocus-method') == 'select') {
+				autoFocusable.first().select();
+			} else {
+				autoFocusable.first().focus();
+			}
 		} else { // No auto focus items - find first input (if any) and use that
 			var cand = $('.modal.show input[type]')
 				.filter((i, el) => ['text', 'email', 'number', 'url'].includes($(el).attr('type')))
