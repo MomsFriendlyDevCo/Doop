b0b00c47ab2166dcc603a4929de821be216085f6
Matt Carter <m@ttcarter.com>
2020-08-06 14:39:38 +1000

    BUGFIX: Better Mac support for sanity checks + centralised disk checking sanity utility

--- skeleton/sanity/dataDiskspace.js
+++ skeleton/sanity/dataDiskspace.js
@@ -3,26 +3,5 @@
 * @param {string} app.config.paths.data Data directory path to check
 * @param {number} [app.config.sanity.minDisk=10] Percentage minimum disk space to warn at
 */
-var exec = require('@momsfriendlydevco/exec');
-var os = require('os');
-var readable = require('@momsfriendlydevco/readable');
 
-if (!['darwin', 'freebsd', 'linux', 'openbsd', 'sunos'].includes(os.platform())) {
-	app.log.warn('Cannot check diskspace on non Unix compatible systems');
-	module.exports = Promise.resolve();
-} else {
-	module.exports = ()=> Promise.resolve()
-		.then(()=> exec(`df -T "${app.config.paths.data}"`, {buffer: true}))
-		.then(res => res.split('\n').slice(1)[0]) // Remove first line (headers)
-		.then(res => res.split(/\s+/)) // Split by whitespace
-		.then(([device, fsType, size, used, avail, usePercent, mounted]) => {
-			freePercent = 100 - parseFloat(usePercent);
-			if (freePercent <= app.config.sanity.minDisk) {
-				return Promise.reject(`Only ${freePercent}% disk remaining - ${readable.fileSize(used)} / ${readable.fileSize(size)} @ ${usePercent} usage for ${mounted}`);
-			} else {
-				return `${readable.fileSize(used)} / ${readable.fileSize(size)} @ ${usePercent} usage for ${mounted}`;
-			}
-		});
-}
-
-module.exports();
+module.exports = ()=> app.utils.sanity.diskSpace(app.config.paths.data, app.config.sanity.minDisk);
