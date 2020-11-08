6c97d792a664589996d9c0cef90088cf1eb28953
Mr-Yellow <mr-yellow@mr-yellow.com>
2020-11-09 10:32:19 +1100

    Removed extra external users

--- skeleton/debug/debug.users.vue
+++ skeleton/debug/debug.users.vue
@@ -36,32 +36,6 @@ module.exports = {
 				</div>
 			</div>
 			<!-- }}} -->
-			<!-- Merged (all) users {{{ -->
-			<div class="form-group row">
-				<label class="col-sm-3 col-form-label">Merged user select</label>
-				<div class="col-sm-6 form-control-plaintext">
-					<digest-select
-						:selected="selectedMerged"
-						field="name"
-						url="/api/users/merged"
-						doc-url="/api/users/merged/:id"
-						search-method="q"
-						class-valid="badge badge-primary"
-						icon-valid="fas fa-user"
-						class-invalid="badge badge-danger"
-						text-invalid="Invalid user"
-						class="col-sm-8 form-control-plaintext"
-						select-text="Search accounts..."
-						title="Search existing accounts"
-						:allow-remove="false"
-						@change="selectedMerged = $event"
-					/>
-				</div>
-				<div class="col-sm-3">
-					<pre>Value: {{selectedMerged || 'none'}}</pre>
-				</div>
-			</div>
-			<!-- }}} -->
 		</div>
 	</div>
 </template>
