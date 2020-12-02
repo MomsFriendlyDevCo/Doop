d0e4a0976065f2346ba5d5b6c1c3c85c2fbaf5e3
Mr-Yellow <mr-yellow@mr-yellow.com>
2020-12-02 11:19:12 +1100

    Only need the middleware directory

---
+++ skeleton/middleware.search/search.list.vue
@@ -0,0 +1,50 @@
+<component>
+module.exports = {
+	route: '/search',
+	created() {
+		this.$sitemap.setTitle(`Search: ${this.$route.query.q}`);
+	}
+};
+</component>
+
+<template>
+	<div>
+		<!-- TODO: Multi-collection search results -->
+		<!--
+		<h4>Collection</h4>
+		<v-table
+			ref="collection"
+			:url="`/api/collection?q=${this.$route.query.q}`"
+			:columns="[
+				{
+					id: 'meta.title',
+					title: 'Title',
+					type: 'text',
+					sortable: true,
+				},
+				{
+					id: 'created',
+					type: 'date',
+					sortable: true,
+					format: function(value) {
+						return this.filter('date')(value, {format: 'YYYY-MM-DD'})
+					}
+				},
+				{
+					id: 'edited',
+					type: 'date',
+					sortable: true,
+					format: function(value) {
+						return this.filter('date')(value, {format: 'YYYY-MM-DD'})
+					}
+				},
+			]"
+			sort="created"
+			:sortAsc="false"
+			:showSearch="false"
+			:cell-href="row => `/collection/${row._id}`"
+			entity="collection"
+		/>
+		-->
+	</div>
+</template>
