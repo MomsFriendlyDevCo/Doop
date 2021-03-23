4af67c19ee063f78dd698d7a7aa30b84974f4138
Matt Carter <m@ttcarter.com>
2021-03-23 16:27:00 +1000

    feat: Moved user avatar display to its own component

--- skeleton/users/user.dropdown.vue
+++ skeleton/users/user.dropdown.vue
@@ -14,7 +14,7 @@ app.component('userDropdown', {
 <template>
 	<li v-if="$session.isLoggedIn" class="dropdown notification-list topbar-dropdown">
 		<a class="nav-link dropdown-toggle nav-user mr-0 waves-effect waves-light" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-			<img :src="$session.data.avatarUrl" alt="user-image" class="rounded-circle">
+			<user-avatar size="32" :user="$session.data"/>
 			<span class="pro-user-name ml-1">
 				{{$session.data.name || $session.data.username || $session.data.email}} <i class="mdi mdi-chevron-down"></i> 
 			</span>
---
+++ skeleton/widgets/user.avatar/user.avatar.vue
@@ -0,0 +1,44 @@
+<script lang="js" frontend>
+/**
+* Display a simple User Avatar sourced from Gravatar.com
+* @param {Object} [user] The user object, takes precedence if present
+* @param {string} [email] The users email
+* @param {number|string} [size=32] The size, in pixels of the Avatar
+* @param {string} [fallback='identicon'] Fallback style to use if the user doesn't have a registered icon. ENUM: 'mp', 'identicon', 'monsterid', 'wavatar', 'retro', 'robohash', 'blank'
+*
+* @see https://en.gravatar.com/site/implement/images/
+*/
+import md5 from 'crypto-js/md5';
+
+app.component('userAvatar', {
+	props: {
+		email: {type: String},
+		user: {type: Object},
+		size: {type: [Number, String], default: 32},
+		fallback: {type: String, default: 'identicon'},
+	},
+	computed: {
+		url() {
+			var email = this.user?.email || this.email;
+			if (!email) { // Not enough data - do nothing
+				return;
+			} else {
+				return 'https://gravatar.com/avatar/'
+					+ md5(email).toString()
+					+ '?'
+					+ `size=${this.size}&`
+					+ `d=${this.fallback}`
+			}
+		},
+	},
+});
+</script>
+
+<template>
+	<img
+		v-if="url"
+		:src="url"
+		alt="user-image"
+		class="rounded-circle"
+	/>
+</template>
