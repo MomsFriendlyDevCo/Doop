fb19562daef890a99a1105336c5917f441c3d77b
Matt Carter <m@ttcarter.com>
2021-03-28 15:52:50 +1000

    fix: MC trying to be smart with node-waves

--- skeleton/vendors/waves.vue
+++ skeleton/vendors/waves.vue
@@ -5,11 +5,10 @@ import Waves from 'node-waves'
 * Inject the material design waves effect to all things that look like buttons
 * Requires 'node-waves' NPM
 */
-app.ready.then(()=>
-	Waves
-		.attach('.btn, .sidebar-menu .nav-link', 'waves-effect waves-block')
-		.attach('a.list-group-item, .dropdown-menu > li > a, .main-menu > li > a', 'waves-effect waves-block')
-		.attach('.btn-ellipsis .btn-circle', 'waves-effect waves-circle waves-block')
-		.init()
-)
+app.ready.then(()=> {
+	Waves.attach('.btn, .sidebar-menu .nav-link', 'waves-effect waves-block');
+	Waves.attach('a.list-group-item, .dropdown-menu > li > a, .main-menu > li > a', 'waves-effect waves-block');
+	Waves.attach('.btn-ellipsis .btn-circle', 'waves-effect waves-circle waves-block');
+	Waves.init();
+})
 </script>
