c22e6be4002d7ba08b1616e2ba2d73a28f6425ae
Matt Carter <m@ttcarter.com>
2021-04-15 06:12:30 +1000

    style: Nicer btn-hover-{danger,success} class handling via SCSS

--- skeleton/theme/bootstrap-extensions/btn-hover.vue
+++ skeleton/theme/bootstrap-extensions/btn-hover.vue
@@ -1,7 +1,21 @@
-<style>
-.btn-hover-danger:hover {
-	background: var(--danger);
-	border: 1px solid var(--danger);
-	color: var(--light) !important;
+<style lang="scss">
+.btn.btn-hover-danger {
+	transition: background 0.5s ease-out, border 0.5s ease-out, color 0.5s ease-out;
+
+	&:hover {
+		background: var(--danger) !important;
+		border: 1px solid var(--danger) !important;
+		color: var(--light) !important;
+	}
+}
+
+
+.btn.btn-hover-success {
+	transition: background 0.5s ease-out, border 0.5s ease-out, color 0.5s ease-out;
+	&:hover {
+		background: var(--success) !important;
+		border: 1px solid var(--success) !important;
+		color: var(--light) !important;
+	}
 }
 </style>
