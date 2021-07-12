e2489cd28cc864d2cd78af973266f63526cd4b39
Matt Carter <m@ttcarter.com>
2021-07-12 16:34:48 +1000

    feat: Added generic <digest-user :user="Object|String"/> widget

---
+++ skeleton/widgets/digest/digest.user.vue
@@ -0,0 +1,44 @@
+<script lang="js" frontend>
+/**
+* Wrapper around <digest/> for user viewing as a simple pill
+* This is really just a shortcut to display the regular digest widgets with a sane setup of properties specifically for users
+*
+* @param {string|Object} user The user to show, can be the entire object or just the ID
+*/
+app.component('digestUser', {
+	props: {
+		user: {type: [String, Object], required: true},
+	},
+	computed: {
+		userId() {
+			return _.isString(this.user) ? this.user
+				: _.isObject(this.user) && this.user._id ? this.user._id
+				: (()=> { throw new Error(`Invalid user type "${this.user}"`) })()
+		},
+	},
+});
+</script>
+
+<template>
+	<digest
+		:url="`/api/users/${userId}`"
+		field="*"
+		:lazy="false"
+		class="d-flex align-items-center"
+		class-valid="badge badge-primary"
+		class-invalid="badge badge-warning"
+		icon-valid="far fa-user"
+		icon-invalid="far fa-exclaimation-triangle"
+		text-invalid="Invalid User"
+		:ignore-errors="true"
+	>
+		<template #loading>
+			<i class="far fa-spinner fa-spin"/>
+			Loading user...
+		</template>
+		<template #display="{data}">
+			<user-avatar :user="userId" size="24" class="d-inline-block mr-1"/>
+			{{data.name}}
+		</template>
+	</digest>
+</template>
