ef3b3221047820a87a90e66feaa2124fb46fd04e
Matt Carter <m@ttcarter.com>
2021-09-15 14:11:28 +1000

    style: More .table-sticky-t{head,foot} fixes

--- skeleton/theme/bootstrap-extensions/table-sticky.vue
+++ skeleton/theme/bootstrap-extensions/table-sticky.vue
@@ -8,58 +8,62 @@ table {
 			background: #FFF;
 			z-index: 9;
 
-			&:first-child {
-				z-index: 11;
-			}
-
-			/* Add borders to floating items {{{ */
-			&:first-child:before {
-				border-left: 2px solid #ddd;
-			}
-
-			&:last-child:before {
-				border-right: 2px solid #ddd;
-			}
-
-			&::before {
-				content: '';
-				position: absolute;
-				top: 0;
-				bottom: 0;
-				left: 0;
-				right: 0;
-				border: {
-					top: 2px solid #ddd;
-					bottom: 2px solid #ddd;
-				}
-			}
-			/* }}} */
-
 			/* Fix .col-verbs collapsing {{{ */
 			&.col-verbs {
 				visibility: visible;
 			}
+			/* }}} */
 		}
 	}
 	/* }}} */
 
-	/* table.table- sticky-tfoot {{{ */
-	&.table-sticky-thead tfoot > tr {
+	/* table.table-sticky-tfoot {{{ */
+	&.table-sticky-tfoot tfoot > tr {
 		& > td, & > th {
-			bottom: -1px;
+			bottom: 0px;
 			position: sticky;
 			background: #FFF;
+			z-index: 9;
+		}
+	}
+	/* }}} */
 
-			&:before {
-				content: '';
-				position: absolute;
-				top: 0;
-				left: 0;
-				right: 0;
-				border-top: 2px solid #ddd;
+	/* Add borders to floating items {{{ */
+	&.table-sticky-thead thead > tr > th,
+	&.table-sticky-thead thead > tr > td,
+	&.table-sticky-tfoot tfoot > tr > th,
+	&.table-sticky-tfoot tfoot > tr > td {
+		&:first-child:before {
+			border-left: 2px solid #ddd;
+		}
+
+		&:last-child:before {
+			border-right: 2px solid #ddd;
+		}
+
+		&::before {
+			content: '';
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			border: {
+				top: 2px solid #ddd;
+				bottom: 2px solid #ddd;
 			}
 		}
 	}
+
+	/* Thinner border widths on tfoot {{{ */
+	&.table-sticky-tfoot tfoot > tr > th,
+	&.table-sticky-tfoot tfoot > tr > td {
+		&::before {
+			border-top-width: 1px;
+			border-bottom-width: 1px;
+		}
+	}
+	/* }}} */
 	/* }}} */
 }
 </style>
