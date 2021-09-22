3bffd5295d92501f9324f3fc64086aedf04a9e91
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-09-22 13:56:54 +1000

    fix: Options may be undefined

--- skeleton/filters/percent.vue
+++ skeleton/filters/percent.vue
@@ -1,3 +1,3 @@
 <script lang="js" frontend>
-app.filter('percent', (value, options) => _.round(Number(value || 0), options.precision || 1) + '%');
+app.filter('percent', (value, options) => _.round(Number(value || 0), options?.precision || 1) + '%');
 </script>
