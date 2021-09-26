32a5e142d139ab040cff296ce62adb5f975ba32c
Matt Carter <m@ttcarter.com>
2021-09-26 14:36:57 +1000

    style: .d-flex-center now also supports .d-inline-flex-center

--- skeleton/theme/bootstrap-extensions/d-flex-center.vue
+++ skeleton/theme/bootstrap-extensions/d-flex-center.vue
@@ -2,6 +2,13 @@
 /* CSS shorthand for absolute central flex layout */
 .d-flex-center {
 	display: flex;
+}
+
+.d-inline-flex-center {
+	display: inline-flex;
+}
+
+.d-flex-center, .d-inline-flex-center {
 	justify-content: center;
 	align-items: center;
 }
