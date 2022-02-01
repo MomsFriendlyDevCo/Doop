9583bdd385ec5e285ee8689754f42da8b3db8dca
Mr-Yellow <mr-yellow@mr-yellow.com>
2022-02-01 16:00:03 +1000

    merge: From CS

--- skeleton/filters/permissionCase.vue
+++
@@ -1,8 +0,0 @@
-<script lang="js" frontend>
-/**
-* Convert a camelCasePermission into `Camel > Case > Permission`
-* @param {string} value The input string to process
-* @returns {string} The formatted sting output
-*/
-app.filter('permissionCase', value => _.startCase(value).split(' ').map(word => `${word} >`).join(' ').replace(/ >$/, ''));
-</script>
